f4257cba4a741aa3f3e9d578f867289e4602648b
homexhsms <112154115+homexhsms@users.noreply.github.com>
2022-12-27 11:18:35 +0530

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,97 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1">
+        <meta name="robots" content="index, follow">
+        <meta name="description" content="Welcome to our Event Booking page!   We are pleased to offer you the opportunity to use our Club House for your event.. . We offer great prices for your event, with the following rates:   . 1 Day = 500rs . 2 Days = 700rs . 3 Days = 1000rs . 4 Days = 1200rs . 5 Days = 1500rs . More than 5 Days = 1700rs  . . If you have any questions, please contact us. . . We are here to provide best experience for making your event a success.. ">
+
+        <meta property="og:title" content="Pay for Event Booking by HomeX">
+        <meta property="og:image" content="https://cdn.razorpay.com/logos/KqtkARfWD6TrOg_large.jpg">
+        <meta property="og:image:width" content="276px">
+        <meta property="og:image:height" content="276px">
+        <meta property="og:description" content="Welcome to our Event Booking page!   We are pleased to offer you the opportunity to use our Club House for your event.. . We offer great prices for your event, with the following rates:   . 1 Day = 500rs . 2 Days = 700rs . 3 Days = 1000rs . 4 Days = 1200rs . 5 Days = 1500rs . More than 5 Days = 1700rs  . . If you have any questions, please contact us. . . We are here to provide best experience for making your event a success.. ">
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Pay for Event Booking by HomeX" />
+        <meta name="twitter:description" content="Welcome to our Event Booking page!   We are pleased to offer you the opportunity to use our Club House for your event.. . We offer great prices for your event, with the following rates:   . 1 Day = 500rs . 2 Days = 700rs . 3 Days = 1000rs . 4 Days = 1200rs . 5 Days = 1500rs . More than 5 Days = 1700rs  . . If you have any questions, please contact us. . . We are here to provide best experience for making your event a success.. " />
+        <meta name="twitter:image" content="https://cdn.razorpay.com/logos/KqtkARfWD6TrOg_large.jpg" />
+
+        <link href="img/1.ico" rel="icon">
+        <link href="img/1.ico">
+        
+
+        <style>
+            body {
+                background-color: #efefef;
+            }
+        </style>
+
+                            
+        <script>
+            var data = {"key_id":"rzp_test_5wBC3qKRlukdhc","is_test_mode":true,"merchant":{"id":"KqtbAEemrwVEgi","name":"HomeX","image":"https:\/\/cdn.razorpay.com\/logos\/KqtkARfWD6TrOg_large.jpg","brand_color":"rgb(35,113,236)","brand_text_color":"#ffffff","branding_variant":"control","optimised_web_vitals":"on","contact_optional":false,"email_optional":false,"support_email":"","support_mobile":"","tnc_link":null},"payment_link":{"id":"pl_KuRT1TJBmXZDTZ","amount":null,"currency":"INR","currency_symbol":"₹","expire_by":null,"times_payable":null,"times_paid":0,"status":"active","status_reason":null,"short_url":"https:\/\/rzp.io\/l\/6KfF3ZYuBr","receipt":null,"title":"Event Booking","description":"{\"value\":[{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"Welcome to our Event Booking page!   We are pleased to offer you the opportunity to use our Club House for your event.\"},{\"insert\":\"\\n\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"We offer great prices for your event, with the following rates:   \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"1 Day = 500rs \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"2 Days = 700rs \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"3 Days = 1000rs \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"4 Days = 1200rs \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"5 Days = 1500rs \"},{\"insert\":\"\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"More than 5 Days = 1700rs  \"},{\"insert\":\"\\n\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"If you have any questions, please contact us. \"},{\"insert\":\"\\n\\n\"},{\"attributes\":{\"color\":\"#353740\",\"bold\":true},\"insert\":\"We are here to provide best experience for making your event a success.\"},{\"insert\":\"\\n\"}],\"metaText\":\"Welcome to our Event Booking page!   We are pleased to offer you the opportunity to use our Club House for your event.. . We offer great prices for your event, with the following rates:   . 1 Day = 500rs . 2 Days = 700rs . 3 Days = 1000rs . 4 Days = 1200rs . 5 Days = 1500rs . More than 5 Days = 1700rs  . . If you have any questions, please contact us. . . We are here to provide best experience for making your event a success.. \"}","support_contact":"8469487913","support_email":"homex.manage@gmail.com","terms":null,"type":"payment","payment_page_items":[{"id":"ppi_KuRT1ZPmVPqG6w","entity":"payment_page_item","payment_link_id":"pl_KuRT1TJBmXZDTZ","item":{"id":"item_KuRT1Zg5EUTDrC","active":true,"name":"Amount","description":null,"amount":null,"unit_amount":null,"currency":"INR","type":"payment_page","unit":null,"tax_inclusive":false,"hsn_code":null,"sac_code":null,"tax_rate":null,"tax_id":null,"tax_group_id":null,"created_at":1671599909},"mandatory":true,"image_url":null,"min_purchase":null,"max_purchase":null,"min_amount":null,"max_amount":null,"settings":{"position":"0"},"plan_id":null,"product_config":null,"quantity_available":null}],"hosted_template_id":null,"udf_jsonschema_id":null,"min_amount_value":100,"amount_formatted":"0.00","expire_by_formatted":null,"settings":{"allow_social_share":"0","udf_schema":"[{\"name\":\"email\",\"title\":\"Email\",\"required\":true,\"type\":\"string\",\"pattern\":\"email\",\"settings\":{\"position\":1}},{\"name\":\"phone\",\"title\":\"Phone\",\"required\":true,\"type\":\"number\",\"pattern\":\"phone\",\"minLength\":\"8\",\"options\":[],\"settings\":{\"position\":2}},{\"name\":\"date\",\"title\":\"Date\",\"required\":true,\"type\":\"string\",\"pattern\":\"date\",\"options\":{\"cmp\":\"date\"},\"settings\":{\"position\":3}}]","checkout_options":{"email":"email","phone":"phone"},"payment_button_label":"Pay","version":"V2","enable_receipt":"1","selected_udf_field":"","enable_custom_serial_number":"0","enable_80g_details":"0","theme":"light"}},"org":{"branding":{"show_rzp_logo":true,"branding_logo":""},"custom_code":"rzp"},"checkout_2_enabled":false,"keyless_header":"api_v1:7g7yL9xDdkkySonwY\/xygZu4d\/FhHR0Jm01FG1CsekTy23yyb5ASiyeUGmiCQQcJMG0TqwCIkr6oE5\/2+OR5A29\/1tYqfQ==","view_preferences":{"exempt_customer_flagging":false,"disclaimer_text_enabled":"on","crossorigin_enabled":"on"},"base_url":"https:\/\/api.razorpay.com","environment":"production"};
+
+            var paymentPageData = data.payment_link;
+            paymentPageData.description = paymentPageData.description ? JSON.parse(paymentPageData.description).value : null;
+
+            var requestParams = [];
+
+            var templateData = {
+                key_id: data.key_id,
+                base_url: data.base_url,
+                is_test_mode: data.is_test_mode,
+                merchant: data.merchant,
+                org: data.org,
+                payment_page_data: data.payment_link,
+                context: {
+                  page_title: data.payment_link.title,
+                  form_title: 'Payment Details'
+                },
+                requestParams: requestParams,
+                view_preferences: data.view_preferences,
+                keyless_header: data.keyless_header,
+                checkout_2_enabled: data.checkout_2_enabled ?? false,
+              };
+        </script>
+
+                    <script>
+                function renderPaymentPage() {
+                    window.RZP.renderApp('paymentpage-container', templateData);
+                }
+            </script>
+            <!-- Temporary polyfill for analytics -->
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=URL%2CURLSearchParams"></script>
+                                                <script src="https://cdn.razorpay.com/static/analytics/bundle.js" defer></script>
+                                <script src="https://cdn.razorpay.com/static/assets/color.js" defer></script>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link href="https://fonts.googleapis.com/css?family=Muli:400,700&display=swap" rel="stylesheet">
+                <script src="https://cdn.razorpay.com/static/assets/quilljs/1.3.6/quill.min.js" defer ></script>
+
+                <script 
+                    src="https://cdn.razorpay.com/static/hosted/wysiwyg.js" 
+                    onload="renderPaymentPage()" 
+                    defer
+                    crossorigin
+                ></script>
+                <link rel="stylesheet" href="https://cdn.razorpay.com/static/assets/social-share/icons.css" />
+                        </head>
+
+    <body>
+        <div id="paymentpage-container">
+                    </div>
+        <!-- Adding checkout scripts after initial load -->
+                    <script>
+                window.addEventListener('load', function() {
+                    setTimeout(function () {
+                        var script = document.createElement("script");
+                        script.src = "https://checkout.razorpay.com/v1/checkout.js";
+                        document.body.appendChild(script);
+                    }, 2000);
+                });
+            </script>
+            </body>
+</html>
